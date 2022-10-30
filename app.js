6c7e14b98842253a811175ef75ef27998f1144fd
Gialdini Javier <jngialdini19@gmail.com>
2022-10-30 18:35:25 -0300

    agrego js y css

---
+++ app.js
@@ -0,0 +1,33 @@
+// Compras de na despensa
+
+/*
+*
+* 3kg de Tomate
+* 1/2 kg de Frijol
+* 1 kg de cebolla
+* 1/4 de chile
+*
+*
+* PRECIOS:
+* Kg de tomate: 16.67
+* Kg de Frijol: 27
+* Kg de cebolla: 18.90
+* kg de chile: 11
+*
+*/
+/*
+const tomate = 16.67
+const frijol = 27
+const cebolla = 18.90
+const chile = 11
+
+let total = (tomate*3) + (frijol/2) + cebolla + (chile/4)
+
+console.log("El total es= " + total.toString())
+*/
+
+let num = prompt("Ingrese un número")
+alert(num*2)
+let rad = 5.3
+let area = Math.PI*rad**2
+alert(area)
